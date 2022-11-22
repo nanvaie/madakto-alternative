b08036698b85aaa473cb970ff49782f838a2e704
Mohammad-Reza Sharifi <mrsharifi76@gmail.com>
2022-11-22 23:48:19 +0330

    feat(Assets): Add Some Style and Override SAP Default Fonts

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue';
 import App from '../vue/App.vue';
 import router from './router/index';
 
+// Style
+import '../scss/app.scss';
+
 createApp(App)
     .use(router)
     .mount('#app');
