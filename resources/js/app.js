5dbe99a01c300bbc91d08345fa349c026458068d
Mohammad-Reza Sharifi <mrsharifi76@gmail.com>
2022-11-22 23:53:17 +0330

    feat(Assets): Use Some Assets Provided by SAP Fiori

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,10 @@ import { setLanguage } from '@ui5/webcomponents-base/dist/config/Language';
 import App from '../vue/App.vue';
 import router from './router/index';
 
+// Assets
+import '@ui5/webcomponents/dist/Assets';
+import '@ui5/webcomponents-fiori/dist/Assets';
+
 // Style
 import '../scss/app.scss';
 
