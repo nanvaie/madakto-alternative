64b8a12a99cb937c3ecf23c37ddefe409b842054
ahmad dehestani <s.ahmad.dehestani@gmail.com>
2023-02-12 18:46:54 +0330

    feature : DepartmentController crud

---
+++ app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Department;
+class DepartmentController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            "name" => ["required", 'min:2', 'max:63'],
+            "user_id" => ["required"],
+
+        ]);
+        $department = new Department();
+        $department->name = $request->name;
+        $department->user_id  = $request->user_id;
+        $department->save();
+
+        return response()->json(["success"], 200);
+    }
+
+    public function edit($id)
+    {
+        $department = Department::find($id);
+        return response()->json([$department], 200);
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            "name" => ["required", 'min:2', 'max:63'],
+            "user_id" => ["required"],
+
+        ]);
+
+        Department::where('id', $request->id)->first()
+            ->update([
+                'name' => $request->name,
+                'user_id' => $request->user_id,
+            ]);
+
+        return response()->json(["success"], 200);
+    }
+}
